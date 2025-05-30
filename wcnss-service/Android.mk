c7ac20cacb78144e23dadc3aa156470f966723ea
plahon <plahon@qti.qualcomm.com>
2025-05-30 23:09:51 +0530

    hardware-wlan: target specific conditional make compilation
    Change-Id: I48dddf8a4bafdc2323a8a316ebc1786ab54e5dc6

--- wcnss-service/Android.mk
+++ wcnss-service/Android.mk
@@ -1,3 +1,4 @@
+ifeq ($(TARGET_BOARD_PLATFORM), monaco)
 ifneq (,$(filter arm aarch64 arm64, $(TARGET_ARCH)))
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
@@ -26,3 +27,4 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wall -Werror
 include $(BUILD_EXECUTABLE)
 endif
+endif #TARGET_BOARD_PLATFORM
