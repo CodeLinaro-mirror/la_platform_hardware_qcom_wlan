0549d6c4497b4eb22a1eb78d557e54612261cb09
Vinay Gannevaram <vganneva@qti.qualcomm.com>
2026-03-26 18:48:47 -0700

    Ensure valid dialog token in bootstrapping handshake
    A non-zero dialog token is a valid token in bootstrapping request, hence
    set a non-zero dialog token in the bootstrapping request and ensure the
    same token is sent in the response.
    
    CRs-Fixed: 4225972
    Change-Id: I789c4ac7ed4852faf4fa03b448e1a530dfd53b4e

--- qcwcn/wifi_hal/nan_i.h
+++ qcwcn/wifi_hal/nan_i.h
@@ -2128,6 +2128,8 @@ struct nan_pairing_peer_info {
     /* capability info in CSIA attribute */
     u8 csia_cap_info;
     struct pasn_auth_frame *frame;
+    /* dialog token in bootstrapping request/response */
+    u8 dialog_token;
 };
 
 struct wpa_secure_nan {
@@ -2173,6 +2175,8 @@ struct wpa_secure_nan {
     struct nan_pairing_peer_info* pending_peer;
     /* device group keys capability info*/
     u8 csia_cap_info;
+    /* dialog token in bootstrapping request */
+    u8 dialog_token;
 };
 
 /***************************************************
